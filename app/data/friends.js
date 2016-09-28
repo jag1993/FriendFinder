16ea910823101e7c584d1f9cf0a081790397fd6d
Jose Alvaro Garcia <jag040393@gmail.com>
2016-09-27 23:31:36 -0700

    Fixed algorithm issue

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ var friends = [
 {
   "name":"Bob",
   "photo":"http://dlisted.com/wp-content/uploads/2015/09/spl1129631_001-copy.jpg",
-  "scores":[5,5,5,5,5,5,5,5,5,5]
+  "scores":[3,3,2,5,3,2,3,5,5,5]
 },
 {
   "name":"Paris",
   "photo":"http://dlisted.com/wp-content/uploads/2015/09/spl1129631_001-copy.jpg",
-  "scores":[5,5,5,5,5,5,5,5,5,5]
+  "scores":[2,3,4,3,4,5,2,5,5,5]
 },
 {
   "name":"Lincoln",
   "photo":"http://dlisted.com/wp-content/uploads/2015/09/spl1129631_001-copy.jpg",
-  "scores":[5,5,5,5,5,5,5,5,5,5]
+  "scores":[5,1,5,5,2,4,3,5,5,5]
 },
 {
   "name":"Lori Beth Denberg",
   "photo":"http://dlisted.com/wp-content/uploads/2015/09/spl1129631_001-copy.jpg",
-  "scores":[5,5,5,5,5,5,5,5,5,5]
+  "scores":[2,5,5,5,1,5,2,5,1,5]
 } 
 ];
 
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,16 +4,39 @@ var path = require('path');
 
 var friends = require('../data/friends.js');
 
-
 module.exports = function(app){
 app.post('/api/friends', function (req, res) {
 	var newFriend = req.body;
 	var newFriendScores = newFriend.scores;
-	var newFriendConvArr = newFriendScores.map(Number)
+	var newFriendConvArr = newFriendScores.map(Number);
+	var numbers = newFriendConvArr;
+	var count = 0;
+	var sum = numbers.reduce(getSum);
+
+
+function getSum(total, num) {
+  		return total + num;
+	}
+
 	req.body.scores = newFriendConvArr;
 	friends.push(newFriend);
 	console.log(friends);
-	res.json(friends[Math.floor(Math.random()*friends.length)]);
+	
+
+	for(i=0;i<friends.length;i++){
+	 var thing = Math.abs(sum - friends[i].scores.reduce(getSum));
+		if(thing<3){
+		count ++;
+		}
+
+	}
+
+	if(count >= friends.length){
+	count -=1;
+	}	
+
+	res.json(friends[count]);
+
 });
 
 app.get('/api/show', function (req, res) {	
@@ -21,3 +44,6 @@ app.get('/api/show', function (req, res) {
 		res.json(friends);
 });
 }
+
+
+
